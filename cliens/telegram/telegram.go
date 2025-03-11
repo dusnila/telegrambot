93b9ef07f39e7301db21aa32c331430c27a0cf13
dusnila <davidovich18062002@gmail.com>
2025-03-11 21:25:35 +0000

    first commit

---
+++ cliens/telegram/telegram.go
@@ -0,0 +1,17 @@
+package telegram
+
+import "net/http"
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+func New(host string, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module telegramBot
+
+go 1.23.4
